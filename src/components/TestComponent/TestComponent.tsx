b7409a71c12ba4df0f44fa1eac1d7b908972dbe2
albertorisucci <risucci@gmail.com>
2020-12-02 11:56:11 -0300

    teste - commit master

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './TestComponent.css'
 
 const TestComponent = () => (
-  <div className="TestComponent">Olá, Mundo!</div>
+  <div className="TestComponent">Olá, Mundo!!!</div>
 )
 
 export default TestComponent
