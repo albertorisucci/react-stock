bd6e6df703eb0ce729a916041f549950b128cf24
albertorisucci <risucci@gmail.com>
2020-12-04 17:54:06 -0300

    Componentização do testComponent e App

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './TestComponent.css'
 
 const TestComponent = () => (
-  <div className="TestComponent">Olá, Mundo!!!</div>
+  <div className="TestComponent">TestComponent</div>
 )
 
 export default TestComponent
