a3c4ad69589b801fc442a42a0d5e67fb467cd5d1
albertorisucci <risucci@gmail.com>
2020-12-06 10:32:32 -0300

    add form component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 import Container from '../../shared/Container';
 import Table, { TableHeader } from '../../shared/Table';
 import Products from '../../shared/Table/Table.mockdata';
+import Form from '../../shared/Form';
+import Input from '../../shared/Input';
+import Button from '../../shared/Button';
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -23,6 +26,29 @@ function App() {
           headers={headers}
           data={Products}
         />
+
+        <Form title="Product form" onSubmit={console.log}>
+          <Input
+            label="Name"
+            placeholder="e.g.: Cookie"
+          />
+          <Input
+            label="Price"
+            type="number"
+            step="0.01"
+            min="0"
+            placeholder="e.g.: 1.25"
+          />
+          <Input
+            label="Stock"
+            type="number"
+            min="0"
+            placeholder="e.g.: 15"
+          />
+          <Button>
+            Submit
+          </Button>
+        </Form>
       </Container>
     </div>
   );
