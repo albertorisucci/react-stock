47dc23eba08fb36f63cde0a26f1c4eee506aac79
albertorisucci <risucci@gmail.com>
2020-12-11 15:36:01 -0300

    update product schema + fix table actions

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -54,7 +54,7 @@ const Table: React.FC<TableProps> = (props) => {
                 .map((item, i) =>
                   item !== '$original'
                     ? <td
-                        key={row.$original.id + i}
+                        key={row.$original._id + i}
                         className={indexedHeaders[item].right ? 'right' : ''}
                       >
                         { row[item] }
@@ -69,21 +69,21 @@ const Table: React.FC<TableProps> = (props) => {
                   {
                     props.onEdit && 
                       <Button 
-                        onClick={() => props.onEdit && props.onEdit(row)}>
+                        onClick={() => props.onEdit && props.onEdit(row.$original)}>
                         Edit
                       </Button>
                   }
                   {
                     props.onDetail && 
                       <Button 
-                        onClick={() => props.onDetail && props.onDetail(row)}>
+                        onClick={() => props.onDetail && props.onDetail(row.$original)}>
                         Detail
                       </Button>
                   }
                   {
                     props.onDelete && 
                       <Button
-                        onClick={() => props.onDelete && props.onDelete(row)}>
+                        onClick={() => props.onDelete && props.onDelete(row.$original)}>
                         Delete
                       </Button>
                   }
